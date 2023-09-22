ec5a640178838aa3f318df629ed789197b9752c5
Jihyun3478 <leejihyun3478@gmail.com>
2023-09-23 01:13:23 +0900

    update Problem1

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,8 +4,8 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        // 포비가 크면 1, 크롱이 크면 2, 같으면 0, 예외 -1
         // 각 자리의 수를 더하거나 곱했을 때, 가장 큰 수
+        // 포비가 크면 1, 크롱이 크면 2, 같으면 0, 예외 -1
 
         String[] pobiArr = new String[2];
         pobiArr[0] = String.valueOf(pobi.get(0));
@@ -34,11 +34,13 @@ class Problem1 {
 
     public static int max(String[] list) {
         int sum = 0, mul = 1, answer1 = 0, answer2 = 0;
+
         for(int i = 0; i < list[0].length(); i++) {
             sum += Character.getNumericValue(list[0].charAt(i));
             mul *= Character.getNumericValue(list[0].charAt(i));
             answer1 = Math.max(sum, mul);
         }
+
         for(int i = 0; i < list[1].length(); i++) {
             sum += Character.getNumericValue(list[1].charAt(i));
             mul *= Character.getNumericValue(list[1].charAt(i));
