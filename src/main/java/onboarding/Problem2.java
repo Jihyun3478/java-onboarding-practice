615891e73fc0227f0924e36ad959978056b2e054
Jihyun3478 <leejihyun3478@gmail.com>
2023-10-18 00:04:26 +0900

    update Problem1

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,19 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        // 처음부터 끝까지 연속된 2개의 문자가 있는지 체크 후, 있다면 둘 다 제거
+        // 연속된 문자가 없을 때까지 위 과정 반복
+        return removeAll(cryptogram);
+    }
+
+    public static String removeAll(String s) {
+        List<String> list = new ArrayList<>(Arrays.asList(s.split("")));
+
+
     }
 }
