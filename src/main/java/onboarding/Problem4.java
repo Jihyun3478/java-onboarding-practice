ecb19c4bad3ca7440326652fd16bccf1be25b6d5
Jihyun3478 <leejihyun3478@gmail.com>
2023-10-18 16:02:34 +0900

    update Problem4

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -6,10 +6,23 @@ public class Problem4 {
      * 📚 기능 목록
      * 1. 엄마의 말씀을 청개구리 사전대로 변환하는 기능
      */
-
     public static String solution(String word) {
-        word = word.replace()
-        String answer = "";
-        return answer;
+        StringBuilder answer = new StringBuilder();
+
+        for(int i = 0; i < word.length(); i++) {
+            answer.append(converseTo(word.charAt(i)));
+        }
+        return answer.toString();
+    }
+
+    public static char converseTo(char letter) {
+        char result = letter;
+        if(Character.isUpperCase(result)) {
+            result = (char)('A' + 'Z' - letter);
+        }
+        else if(Character.isLowerCase(result)) {
+            result = (char)('a' + 'z' - letter);
+        }
+        return result;
     }
 }
