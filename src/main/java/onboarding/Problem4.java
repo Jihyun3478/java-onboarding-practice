62f1e3cf21982c7acf3433770105c32c38439ec6
Jihyun3478 <leejihyun3478@gmail.com>
2023-10-18 10:34:46 +0900

    update Problem4

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,7 +1,14 @@
 package onboarding;
 
 public class Problem4 {
+
+    /*
+     * 📚 기능 목록
+     * 1. 엄마의 말씀을 청개구리 사전대로 변환하는 기능
+     */
+
     public static String solution(String word) {
+        word = word.replace()
         String answer = "";
         return answer;
     }
